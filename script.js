32990a39af5b7048371b9af071386aead31bc768
wikawa <wilsonwacera@gmail.com>
2026-04-30 17:06:16 +0300

    Initial Insight Ledger website

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector(".menu-toggle");
+const menu = document.querySelector("#site-menu");
+
+if (menuButton && menu) {
+  menuButton.addEventListener("click", () => {
+    const isOpen = menu.classList.toggle("is-open");
+    menuButton.setAttribute("aria-expanded", String(isOpen));
+  });
+}
